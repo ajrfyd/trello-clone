b1b7ae936134cebbe084160486bca483b40d7764
ajrfyd <ajrfyd@naver.com>
2022-06-08 00:17:12 +0900

    Create: Initial Setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { ColumnContainer, ColumnTitle, CardContainer } from './styles';
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ColumnContainer>
+      <ColumnTitle>Todo: </ColumnTitle>
+      <CardContainer>First Item</CardContainer>
+      <CardContainer>Second Item</CardContainer>
+      <CardContainer>Third Item</CardContainer>
+    </ColumnContainer>
+  )
 }
 
-export default App;
